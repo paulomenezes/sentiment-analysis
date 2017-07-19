5f3d1af6b977af5741e6023ecdf46706fd72e9f4
Paulo Menezes <paulo.hgmenezes@gmail.com>
2017-07-19 18:19:55 -0300

    Calculate scores

--- main.js
+++ main.js
@@ -4,10 +4,13 @@ const nlp = require('compromise');
 const wordNet = require('wordnet');
 const WordNet = require('node-wordnet');
 const sw = require('sentiword');
+const xlsx = require('node-xlsx');
 
 const trainingData = __dirname + '/data/training/ABSA16_Restaurants_Train_SB1_v2.xml';
 
 const wordnet = new WordNet();
+
+const inquirer = xlsx.parse(__dirname + '/data/training/inquireraugmented.xls'); // parses a file
 
 fs.readFile(__dirname + '/data/training/WordNet2/WordNet Words & Phrases.CAT', (err, data) => {
   const lexical = {};
@@ -44,34 +47,7 @@ fs.readFile(__dirname + '/data/training/WordNet2/WordNet Words & Phrases.CAT', (
         const sentenceText = sentence.text[0].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
 
         let text = nlp(sentenceText);
-        let sentiText = sw(sentenceText);
-
-        let sentences = [];
-        let newSentence = '';
-        let add = false;
-
-        text.list[0].terms.forEach(term => {
-          if (term.tags.Conjunction) {
-            sentences.push(newSentence);
-            newSentence = '';
-            add = true;
-          } else {
-            newSentence += term.text + ' ';
-            add = false;
-          }
-        });
-
-        if (!add) {
-          sentences.push(newSentence);
-        }
-
-        sentences.forEach(sentence => {
-          // nlp(sentence).debug();
-          console.log('---------');
-          console.log(sentence);
-          console.log(sw(sentence));
-          console.log('---------');
-        });
+        //let sentiText = sw(sentenceText);
 
         const words = [];
         const lemmas = {};
@@ -98,6 +74,89 @@ fs.readFile(__dirname + '/data/training/WordNet2/WordNet Words & Phrases.CAT', (
                 opinions.push(opinion['$'].target + ' - ' + opinion['$'].polarity);
               });
 
+              let sentences = [];
+              let newSentence = '';
+              let add = false;
+
+              text.list[0].terms.forEach(term => {
+                if (term.tags.Conjunction) {
+                  sentences.push(newSentence);
+                  newSentence = '';
+                  add = true;
+                } else {
+                  newSentence += term.text + ' ';
+                  add = false;
+                }
+              });
+
+              if (!add) {
+                sentences.push(newSentence);
+              }
+
+              let sentencesSW = [];
+              sentences.forEach(sentence => {
+                // nlp(sentence).debug();
+                //console.log('---------');
+                //console.log(sentence);
+                let sentencePieces = sentence.trim().split(' ');
+                let sentencePiecesSW = {};
+                sentencePieces.forEach(piece => {
+                  sentencePiecesSW[piece] = sw(piece);
+                });
+
+                sentencesSW.push(sentencePiecesSW);
+              });
+
+              sentencesSW.forEach(s => {
+                let pos = 0;
+                let neg = 0;
+                let sen = '';
+
+                Object.keys(s).forEach(key => {
+                  let positive = 0;
+                  let negative = 0;
+
+                  if (s[key].words.length === 0) {
+                    let exist = false;
+                    for (var j = 2; j < inquirer[0].data.length; j++) {
+                      var line = inquirer[0].data[j];
+                      if (line[0] && line[0].toLowerCase() === key.toLowerCase()) {
+                        exist = true;
+                        if (line[2] || line[4] || line[5] || line[8]) positive += 1;
+                        if (line[3] || line[6] || line[7] || line[10]) negative += 1;
+                      }
+                    }
+
+                    if (!exist) {
+                      // console.log(0, 0);
+                    }
+                  } else {
+                    positive += s[key].positive;
+                    negative += s[key].negative;
+                  }
+
+                  sen += key + ' ';
+                  pos += positive;
+                  neg += negative;
+
+                  // console.log('WORD:', key, 'Positive', positive, 'Negative', negative);
+                  // console.log();
+                });
+
+                console.log(sen);
+                console.log(pos, neg);
+              });
+
+              // for (var i = 0; i < words.length; i++) {
+              //   var word = words[i];
+              //   for (var j = 2; j < inquirer[0].data.length; j++) {
+              //     var line = inquirer[0].data[j];
+              //     if (line[0] && line[0].toLowerCase() === word) {
+              //       console.log(word, line[1]);
+              //     }
+              //   }
+              // }
+
               // console.log(sentiText);
               // console.log('sentiment', sentiFullText.sentiment);
               // for (var i = 0; i < words.length; i++) {
@@ -111,7 +170,7 @@ fs.readFile(__dirname + '/data/training/WordNet2/WordNet Words & Phrases.CAT', (
               //   }
               // }
 
-              console.log(sentenceText);
+              //console.log(sentenceText);
               console.log('words', words);
               console.log('opinions', opinions);
               console.log();
